087579391d1b56b81617121628fc83aa72e6a71b
yuvanis <uvnistratov@gmail.com>
2022-09-30 11:35:56 +0300

    feat: add weather forecast

--- src/lwc/weatherForecast/weatherForecast.js
+++ src/lwc/weatherForecast/weatherForecast.js
@@ -2,8 +2,57 @@
  * Created by yurynistratau on 29.09.22.
  */
 
-import {LightningElement} from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getWeatherForecast from '@salesforce/apex/WeatherForecastController.getWeatherForecast';
+
 
 export default class WeatherForecast extends LightningElement {
+
+    @api temperature;
+    @api wind;
+    @api humidity;
+    @api isValidData;
+    @track city;
+
+    connectedCallback() {
+        this.city = 'Minsk';
+        this.isValidData = true;
+        this.getCallback();
+    }
+
+    handleKeyUp(evt) {
+        const isEnterKey = evt.keyCode === 13;
+        if (isEnterKey) {
+            this.city = evt.target.value;
+            this.getCallback();
+        }
+    }
+
+    getCallback(){
+        getWeatherForecast({city: this.city})
+            .then(data => {
+                if (data.temperature === undefined) {
+                    this.temperature = '';
+                    this.wind = '';
+                    this.humidity = '';
+                    this.isValidData = false;
+                } else {
+                    this.temperature = `${Math.round(data.temperature)}°C`;
+                    this.wind = `Wind speed: ${Math.round(data.wind)} m/s`;
+                    this.humidity = `Humidity: ${Math.round(data.humidity)}%`;
+                    this.isValidData = true;
+                }
+
+            })
+            .catch(error => {
+                    new ShowToastEvent({
+                        title: "Error",
+                        message: 'Invalid data',
+                        variant: "error",
+                    });
+                }
+            )
+    }
 
 }
